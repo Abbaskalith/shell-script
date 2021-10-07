9385cc57a89f964c049060af16666ef218861ec3
Abbaskalith <91032092+Abbaskalith@users.noreply.github.com>
2021-10-07 11:15:16 +0530

    Program file added

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+        public static void Addition()
+        { 
+                Console.WriteLine("Addition")
+        }
+    }
+}
